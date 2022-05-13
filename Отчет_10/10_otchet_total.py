24a9310ceec071f67aa044f5654a1487d24506bf
Alexander Brezhnev <brezhnev.aleksandr@gmail.com>
2022-05-13 10:37:36 +0200

    13.05.2022 - изменение отчета №10.

--- Отчет_10/10_otchet_total.py
+++ Отчет_10/10_otchet_total.py
@@ -142,9 +142,16 @@ select case
        date(cl_1.date_entered)            as calls_date,
        case
            when cl.user_id_c is null or cl.user_id_c = '' or cl.user_id_c = ' ' then 'unknown_id'
-           else cl.user_id_c end          as super
+           else cl.user_id_c end          as super,
+       case
+           when city_c is null then concat(town_c, 'OBL')
+           when city_c in ('', ' ') then concat(town_c, 'OBL')
+           else city_c
+           end                            as city
 from suitecrm.calls_cstm as cl
          left join suitecrm.calls as cl_1 on cl.id_c = cl_1.id
+         left join suitecrm.contacts on cl.asterisk_caller_id_c = contacts.phone_work
+         left join suitecrm.contacts_cstm on contacts_cstm.id_c = contacts.id
 where cl.result_call_c = 'refusing'
   and cl_1.date_entered >= '2022-01-01';
 """
