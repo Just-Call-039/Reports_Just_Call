9303c6740c3c20b6677f930374cc7a44ad1b0c67
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-02-16 13:22:48 +0200

    Запрос between.

--- Отчет_25/SQL/Total_calls_between.sql
+++ Отчет_25/SQL/Total_calls_between.sql
@@ -14,4 +14,4 @@ select call_date + interval 2 hour as my_date,
        was_repeat,
        phone
 from suitecrm_robot.jc_robot_log
-where date(call_date) between '2022-12-09' and '2022-12-12';
+where date(call_date) between '2023-02-13' and '2023-02-14';
