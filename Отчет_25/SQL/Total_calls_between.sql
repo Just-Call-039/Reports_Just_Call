551c6fe97c0000e2e940379fa2924335c96456d2
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-02-20 14:49:52 +0200

    Восстановил 25 на новой системе.

--- Отчет_25/SQL/Total_calls_between.sql
+++ Отчет_25/SQL/Total_calls_between.sql
@@ -14,4 +14,4 @@ select call_date + interval 2 hour as my_date,
        was_repeat,
        phone
 from suitecrm_robot.jc_robot_log
-where date(call_date) between '2023-02-13' and '2023-02-14';
+where date(call_date) between '2023-02-17' and '2023-02-19';
