57d1024af6bf0e3dcee3307b94b34de95ff0b8b5
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-03-03 17:00:22 +0200

    Отчет по шагам - новая версия.

--- steps_report/test.py
+++ steps_report/test.py
@@ -24,32 +24,36 @@ def reset_step(my_str):
         return my_str[-1]
 
 
-# path = r'D:\test.csv'
-# new_file = r'D:\test_new.csv'
 files_from_sql = r'D:\Отчеты\steps_report\files\files_from_sql'
-files_to_report = r'D:\Отчеты\steps_report\files\main_folder'
-main_calls = rf'{files_from_sql}\2023_3_2.csv'
-calls_to_report = rf'{files_to_report}\2023_3_2.csv'
+main_folder = r'D:\Отчеты\steps_report\files\main_folder'
+uniqueid_medium_folder = r'D:\Отчеты\steps_report\files\uniqueid_medium_folder'
 
+name = '2023_3_2.csv'
 
-df = pd.read_csv(main_calls, sep=';', encoding='utf-8')
+main_calls_raw = rf'{files_from_sql}\{name}'
+main_calls_true = rf'{main_folder}\{name}'
+medium_step_file = rf'{uniqueid_medium_folder}\{name}'
+
+df = pd.read_csv(main_calls_raw, sep=';', encoding='utf-8')
 df.fillna('unknown', inplace=True)
-df['route'] = df['route'].apply(is_number_route)
-df['medium_step'] = df['route'].apply(medium_step)
 df['reset_step'] = df['route'].apply(reset_step)
-df = df.explode('medium_step')
+
+df_uniqueid = pd.DataFrame()
+df_uniqueid['uniqueid'] = df['uniqueid']
+df_uniqueid['route'] = df['route'].apply(is_number_route)
+df_uniqueid['medium_step'] = df_uniqueid['route'].apply(medium_step)
+df_uniqueid = df_uniqueid.explode('medium_step')
+
 # df['group_uniq_id'] = df.groupby(['uniqueid']).cumcount() + 1
 # df_new = df.explode('route')
 
-df.to_csv(calls_to_report, sep=';', index=False, encoding='utf-8')
+df.to_csv(main_calls_true, sep=';', index=False, encoding='utf-8')
+df_uniqueid.to_csv(medium_step_file, sep=';', index=False, encoding='utf-8')
 
 print(df.head())
 print(df.info())
 print(df.shape)
-print(df['route'])
 
-# df.to_csv(new_file, sep=';', index=False)
-# df_new.to_csv(r'D:\wtf.csv', sep=';', index=False)
 print('-' * 50)
 
-# print(df_new.head())
+print(df_uniqueid.head())
