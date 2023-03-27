e975e24dc5c09282dc960cf24045c6c72f693bc1
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-03-27 15:19:25 +0200

    Группировка очередей - добавил комментарии.

--- Отчет_25/grouping_of_queues.py
+++ Отчет_25/grouping_of_queues.py
@@ -12,12 +12,16 @@ SCOPES = [
     'https://www.googleapis.com/auth/drive'
 ]
 
+# Ключ - json файл. Формируется в аккаунте гугла.
 json_file = r'D:\Отчеты\not_share\bionic-client-381907-8eb5ac3d5272.json'
+
 credentials = service_account.Credentials.from_service_account_file(json_file, scopes=SCOPES)
 spreadsheet_service = build('sheets', 'v4', credentials=credentials)
 drive_service = build('drive', 'v3', credentials=credentials)
 
+# ИД таблицы - берется из адреса.
 SAMPLE_SPREADSHEET_ID = '1AJN3WDVuSu_y1eEss7Q7rT2a0Zyk2azc0GmOp7I9HQc'
+# Необходимые колонки из таблицы.
 SAMPLE_RANGE_NAME = 'A:C'
 
 sheet = spreadsheet_service.spreadsheets()
@@ -30,12 +34,14 @@ values = result.get('values', [])
 
 today = datetime.date.today()
 
+# Создаем датафрейм с данными из таблицы.
 df = pd.DataFrame(values[1:], index=[i for i in range(len(values) - 1)],
                   columns=['queue', 'group', 'project'])
 
+# Добавляем текущую дату в датафрейм.
 df['date_add'] = today
 
-print(df)
+# print(df)
 
 pd.options.mode.chained_assignment = None
 
@@ -43,5 +49,6 @@ host, user, password = connect_db('Click')
 client = Client(host=host, port='9000', user=user, password=password,
                 database='suitecrm_robot_ch', settings={'use_numpy': True})
 
+# Отправка датафрейма в БД.
 client.insert_dataframe('INSERT INTO suitecrm_robot_ch.grouping_of_queues VALUES',
                         df[['date_add', 'queue', 'group', 'project']])
