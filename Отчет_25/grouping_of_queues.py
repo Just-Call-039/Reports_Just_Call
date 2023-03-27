96b6b59e7a3283f1a453f91f46ae86e332847119
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-03-27 11:41:15 +0200

    25 отчет - группировка очередей
    Добавил возможность выгрузки данных из гугл таблиц в датафрейм.

---
+++ Отчет_25/grouping_of_queues.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+import datetime
+import pandas as pd
+
+SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+json_file = r'D:\Отчеты\not_share\bionic-client-381907-8eb5ac3d5272.json'
+credentials = service_account.Credentials.from_service_account_file(json_file, scopes=SCOPES)
+spreadsheet_service = build('sheets', 'v4', credentials=credentials)
+drive_service = build('drive', 'v3', credentials=credentials)
+
+SAMPLE_SPREADSHEET_ID = '1AJN3WDVuSu_y1eEss7Q7rT2a0Zyk2azc0GmOp7I9HQc'
+SAMPLE_RANGE_NAME = 'A:C'
+
+sheet = spreadsheet_service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range=SAMPLE_RANGE_NAME).execute()
+values = result.get('values', [])
+
+# print(result)
+# print(*[i for i in values], sep='\n')
+
+today = datetime.date.today()
+
+lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks']
+
+df = pd.DataFrame(values[1:], index=[today for _ in range(len(values) - 1)],
+                  columns=['Очередь', 'Группировка', 'Проект (набирающая очередь)'])
+print(df)
