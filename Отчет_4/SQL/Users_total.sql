c0b6480fda7380c5fc1dbbc93649915d79bdcfdd
Ragnarok-039 <63465995+Ragnarok-039@users.noreply.github.com>
2023-04-13 09:21:36 +0200

    Отчет 4 - добавил сип для пользователей

--- Отчет_4/SQL/Users_total.sql
+++ Отчет_4/SQL/Users_total.sql
@@ -1,3 +1,10 @@
-select id, date_entered, first_workday_c, dismissal_date_c, last_login_c, user_id_c, penalty_c
+select id,
+       date_entered,
+       first_workday_c,
+       dismissal_date_c,
+       last_login_c,
+       user_id_c,
+       penalty_c,
+       asterisk_ext_c as sip
 from suitecrm.users as u
          left join suitecrm.users_cstm as u_c on u.id = u_c.id_c;
